d61a5c3549776e738e0f15ad399087eeb3a9c4d9
Mashal <zyadomar112@gmail.com>
2024-02-01 14:08:13 +0200

    first update

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback)
+    }
+}
+// navbar toggle
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNav = function () {
+    navbar.classList.toggle("active");
+    navbarToggler.classList.toggle("active");
+}
+addEventOnElem(navbarToggler, "click", toggleNav);
+
+const closeNav = function () {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+}
+addEventOnElem(navbarLinks, "click", closeNav);
+
+
+// header
+
+const header = document.querySelector("[data-header]")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 100) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+});
+
